aa0bc47628217fef7e73c5cfa5fc7dbe7462a08c
Hardik kanzariya <hardikkanzariya620@gmail.com>
2025-03-17 14:23:29 +0530

    Project-5-MOVIE PROJECTS 13-03-2025

--- Project-5-Movie-Project/routers/movie.routes.js
+++ Project-5-Movie-Project/routers/movie.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const movieController = require("../controllers/movieController");
+const movieController = require("../controller/movie.controller");
 const Movie = require("../model/movie-model");
 
 router.get("/", movieController.getMovies);
